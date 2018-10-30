10ae22ad4dac64e9bedb62e2473d1df79edeafb5
klinsmann Lopes <klinsmann.lopes@geq.com.br>
2018-10-30 14:49:56 -0300

    criando plugin

---
+++ wp-content/plugins/meu-plugin/meu-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Plugin name: Meu plugin
+    Plugin uri:
+    Description: Esse e um plugin de teste
+    Version: 1.0
+    Author: Adam Silva
+    Author uri:
+    License: GPLv2 or later
+ */
+
+
